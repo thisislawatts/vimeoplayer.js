794dc0ee392cde4a7e52d0b4ed23d4135c5b5ba9
thisislawatts <luke@thisis.la>
2015-04-29 22:44:21 +0100

    Adds workflow for minified version

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
 var gulp = require('gulp');
 	plugins = require('gulp-load-plugins')();
 
-gulp.task('build')
+gulp.task('build', function() {
+	gulp.src('vimeo.js')
+	.pipe( plugins.stripDebug() )
+	.pipe( plugins.uglify('vimeo.min.js') )
+	.pipe( gulp.dest('dist') )
+});
